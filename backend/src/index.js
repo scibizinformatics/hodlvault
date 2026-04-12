18cef73db9119043d6a21b7a807c61e6014ca51a
Edo_Tensei <baliteeddmark@gmail.com>
2026-04-12 14:57:06 +0800

    git init backend

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import app from './app.js'
+dotenv.config({
+  path: './.env',
+})
+
+const startServer = async () => {
+  try {
+    await connectDB()
+    app.on('error', (error) => {
+      console.error('Server error:', error)
+      throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error('Server failed to start:', error)
+  }
+}
+
+startServer()

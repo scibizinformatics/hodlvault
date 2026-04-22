3c764002738b12d6bb7cbb99409e0192f209ceb8
Edo_Tensei <baliteeddmark@gmail.com>
2026-04-22 11:56:15 +0800

    feat: stable clean state, working, next target auto delete vault

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,10 @@ const app = express() // create an express app
 app.use(express.json())
 
 // routes import
-import postRouter from './routes/post.route.js'
 import vaultRouter from './routes/vault.route.js'
 import walletPreferencesRouter from './routes/wallet-preferences.route.js'
 
 // routes declaration
-app.use('/api/v1/posts', postRouter)
 app.use('/api/v1/vaults', vaultRouter)
 app.use('/api/v1/wallet/preferences', walletPreferencesRouter)
 
